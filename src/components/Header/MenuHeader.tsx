e556ac1eaffe9ece27359c22a0860a0ffaca83e5
ParsaProg <parsashaabani3@gmail.com>
2025-12-12 00:22:25 +0330

    MenuHeader.tsx > main menu items header changed

--- src/components/Header/MenuHeader.tsx
+++ src/components/Header/MenuHeader.tsx
@@ -15,8 +15,8 @@ export default function MenuHeader() {
   return (
     <motion.div animate={{
         top: scrollUp ? "128px" : 0,
-      }}
-      transition={{ duration: 0.3 }} className="fixed top-32 w-full border-b border-b-neutral-300">
+      }}    
+      transition={{ duration: 0.3 }} className="bg-[#ffffff57] backdrop-blur-3xl fixed top-32 w-full border-b border-b-neutral-300">
       <div className="py-3 sm:w-[80%] w-[90%] flex items-center mx-auto text-xl gap-x-5">
         <motion.div whileTap={{ scale: 0.95 }}>
           <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
