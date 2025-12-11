47e795a71ed9825cae2560046c0a6486dc2a7e7b
ParsaProg <parsashaabani3@gmail.com>
2025-12-12 00:22:41 +0330

    NewSlider added to landing page

---
+++ src/components/landing/NewsSlider.tsx
@@ -0,0 +1,7 @@
+import LandingSectionsTitle from "@/src/ui/landingSectionsTitle";
+
+export default function NewsSlider(){
+    return <div className="flex flex-col items-start gap-y-3">
+        <LandingSectionsTitle title="اخبار امروز" />
+    </div>
+}
