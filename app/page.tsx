46d1198cd9bc8902c11e7c08ab047c368af662ee
ParsaProg <parsashaabani3@gmail.com>
2025-12-12 00:22:04 +0330

    page.tsx changes main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import HotestNews from "@/src/components/landing/HotestNews";
+
 export default function Home(){
-  return <div className="w-full">
-    
+  return <div className="mx-auto sm:w-[80%] w-[90%]">
+    <div className="flex items-start gap-x-10 ">
+      <HotestNews />
+    </div>
   </div>
 }
