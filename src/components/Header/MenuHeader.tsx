00d2d3dab9d0acb97489064d3d827c11681851ac
ParsaProg <parsashaabani3@gmail.com>
2025-12-13 21:04:08 +0330

    MenuHeader.tsx added abd cgabfed the mai backdround and blur

--- src/components/Header/MenuHeader.tsx
+++ src/components/Header/MenuHeader.tsx
@@ -16,8 +16,8 @@ export default function MenuHeader() {
     <motion.div animate={{
         top: scrollUp ? "128px" : 0,
       }}    
-      transition={{ duration: 0.3 }} className="bg-[#ffffff57] backdrop-blur-3xl fixed top-32 w-full border-b border-b-neutral-300">
-      <div className="py-3 sm:w-[80%] w-[90%] flex items-center mx-auto text-xl gap-x-5">
+      transition={{ duration: 0.3 }} className="z-100000 bg-[#ffffffa1] backdrop-blur-sm fixed top-32 w-full border-b border-t border-b-neutral-300 border-t-neutral-300">
+      <div className="py-3 w-[90%] flex items-center mx-auto text-xl gap-x-5">
         <motion.div whileTap={{ scale: 0.95 }}>
           <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
             <Users2Icon />
