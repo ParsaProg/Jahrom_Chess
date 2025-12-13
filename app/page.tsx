267b98f28efeee146f38a518f3f693a93124cc53
ParsaProg <parsashaabani3@gmail.com>
2025-12-13 21:03:45 +0330

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import ChessRefrences from "@/src/components/landing/ChessRefrences";
 import HotestNews from "@/src/components/landing/HotestNews";
+import NewsList from "@/src/components/landing/NewsList";
+import NewsSlider from "@/src/components/landing/NewsSlider";
 
 export default function Home(){
-  return <div className="mx-auto sm:w-[80%] w-[90%]">
-    <div className="flex items-start gap-x-10 ">
+  return <div className="flex flex-col gap-y-14 mx-auto w-[90%]">
+    <div className="flex items-start w-full gap-x-5 ">
       <HotestNews />
+      <NewsSlider />
+      <NewsList />
     </div>
+    <ChessRefrences />
+    <div className="mt-100"></div>
   </div>
 }
