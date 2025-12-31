716f6a71e73d6ec16eee3a7f989a6aa6c09f9362
ParsaProg <parsashaabani3@gmail.com>
2025-12-31 15:45:54 +0330

    LandingSectiontitle.tsx, main component that change for the title of all component sections in the landing page

--- src/ui/landingSectionsTitle.tsx
+++ src/ui/landingSectionsTitle.tsx
@@ -1,8 +1,23 @@
-export default function LandingSectionsTitle({ title }: { title: string }) {
+import Link from "next/link";
+
+export default function LandingSectionsTitle({
+  title,
+  isBetween,
+  openLink
+}: {
+  isBetween?: boolean;
+  title: string;
+  openLink?: string
+}) {
   return (
-    <div className="flex items-center gap-x-3">
-      <div className="bg-blue-600 h-9 w-2.5 rounded-sm"></div>
-      <h1 className="font-bold text-2xl">{title}</h1>
+    <div
+      className={`flex items-center ${isBetween && "w-full justify-between"}`}
+    >
+      <div className="flex items-center  gap-x-3">
+        <div className="bg-blue-600 h-9 w-2.5 rounded-sm"></div>
+        <h1 className="font-bold text-2xl">{title}</h1>
+      </div>
+      {isBetween && <Link href={openLink!} className="text-blue-600 font-bold text-md cursor-pointer">مشاهدۀ همه</Link>}
     </div>
   );
 }
