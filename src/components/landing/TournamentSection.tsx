536168cd07e7aa9e2e1ab6722f5865085e1447fa
ParsaProg <parsashaabani3@gmail.com>
2025-12-31 15:44:59 +0330

    TournamentSection.tsx > the main landing title section edited

---
+++ src/components/landing/TournamentSection.tsx
@@ -0,0 +1,9 @@
+import LandingSectionsTitle from "@/src/ui/landingSectionsTitle";
+
+export default function TournamentLandingSection() {
+  return (
+    <div className="w-full">
+      <LandingSectionsTitle isBetween openLink="/tournaments" title={"گزارش مسابقات"} />
+    </div>
+  );
+}
