d719f9bf1a198be284deda0e065c566a2e4299f9
ParsaProg <parsashaabani3@gmail.com>
2026-02-06 18:43:44 +0330

    Menu header updates

--- src/components/Header/MenuHeader.tsx
+++ src/components/Header/MenuHeader.tsx
@@ -3,6 +3,7 @@
 import {
   ChessKingIcon,
   Image,
+  Newspaper,
   PhoneIncomingIcon,
   Star,
   Users2Icon,
@@ -11,44 +12,53 @@ import { motion } from "framer-motion";
 import useScrollDirection from "@/src/hooks/useScrollDirection";
 
 export default function MenuHeader() {
-    const scrollUp = useScrollDirection();
+  const scrollUp = useScrollDirection();
   return (
-    <motion.div animate={{
+    <motion.div
+      animate={{
         top: scrollUp ? "128px" : 0,
-      }}    
-      transition={{ duration: 0.3 }} className="z-100000 bg-[#ffffffdd] backdrop-blur-sm fixed top-32 w-full border-b border-t border-b-neutral-300 border-t-neutral-300">
-      <div className="py-3 w-[90%] flex items-center mx-auto text-xl gap-x-5">
+      }}
+      transition={{ duration: 0.3 }}
+      className=" z-100000 bg-[#ffffffdd] backdrop-blur-sm fixed top-32 w-full border-b border-t border-b-neutral-300 border-t-neutral-300"
+    >
+      <div className="py-3 w-[90%] flex items-center mx-auto text-lg gap-x-3">
         <motion.div whileTap={{ scale: 0.95 }}>
-          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
-            <Users2Icon />
+          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <Newspaper size={20} />
+            مشاهدۀ اخبار
+          </div>
+        </motion.div>
+        <motion.div whileTap={{ scale: 0.95 }}>
+          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <Users2Icon size={20} />
             مشخصات اعضای هیات شطرنج
           </div>
         </motion.div>
 
         <motion.div whileTap={{ scale: 0.95 }}>
-          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
-            <Star />
+          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <Star size={20} />
             درباره‌ی ما
           </div>{" "}
         </motion.div>
         <motion.div whileTap={{ scale: 0.95 }}>
-          <div className="cursor-pointer flex items-center gap-x-3 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
-            <PhoneIncomingIcon className="-rotate-90" />
+          <div className="cursor-pointer flex items-center gap-x-3 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <PhoneIncomingIcon size={20} className="-rotate-90" />
             تماس با هیات شطرنج
           </div>
         </motion.div>
 
         <motion.div whileTap={{ scale: 0.95 }}>
           {" "}
-          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
-            <ChessKingIcon />
+          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <ChessKingIcon size={20} />
             تاریخچه‌ی مسابقات جهرم
           </div>
         </motion.div>
 
         <motion.div whileTap={{ scale: 0.95 }}>
-          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-4">
-            <Image />
+          <div className="cursor-pointer flex items-center gap-x-2 transition-all duation-200 hover:bg-blue-600 hover:text-white rounded-xl px-3 py-3">
+            <Image size={20} />
             گالری تصاویر
           </div>
         </motion.div>
