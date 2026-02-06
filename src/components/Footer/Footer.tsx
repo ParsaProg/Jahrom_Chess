4c3db2579f2e791762ebcb9160acce08ae9e0d36
ParsaProg <parsashaabani3@gmail.com>
2026-02-06 18:43:03 +0330

    Update the text of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer(){
     return <div className="py-5 mt-10 w-full border-t border-t-neutral-300 flex text-center font-bold">
-        <h1 className="text-center w-full">ساخته شده توسط پارسا شعبانی</h1>
+        <h1 className="text-center w-full">هیات شطرنج شهرستان جهرم</h1>
     </div>
 }
