a812b7cd49a0c18050c23fd36c501b9313bd6e99
ParsaProg <parsashaabani3@gmail.com>
2026-02-10 02:10:53 +0330

    interfaces/regular-card-news.tsx component for clean and structural code

---
+++ src/interfaces/regular-news-card.ts
@@ -0,0 +1,10 @@
+export interface RegularNewsContainer {
+  title: string;
+  description: string;
+  picture: string;
+  author: string;
+  date: string;
+  cat: string;
+  isInGrid?: boolean;
+
+}
