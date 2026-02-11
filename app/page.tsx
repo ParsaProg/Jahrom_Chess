771b1c0bdbeb98c41cd236efa651742fe0faa70d
ParsaProg <parsashaabani3@gmail.com>
2026-02-12 00:11:39 +0330

    page.tsx main home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,12 @@ import TournamentLandingSection from "@/src/components/landing/TournamentSection
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-14 mx-auto w-[90%]">
-      <div className="flex flex-wrap items-start w-full gap-5">
-        <HotestNews />
+      <div className="flex flex-col xl:flex-row flex-wrap items-start justify-center xl:justify-between w-full gap-5">
+        <div className="flex items-start gap-5 w-full max-[777px]:flex-col xl:w-[750px]">
+          <HotestNews />
+          <NewsList />
+        </div>
         <NewsSlider />
-        <NewsList />
       </div>
       <ChessRefrences />
       <GallerySection />
