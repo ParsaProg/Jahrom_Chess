1965570c339cc5c13b00e7f95adfac694ff4c6de
ParsaProg <parsashaabani3@gmail.com>
2026-02-12 00:12:10 +0330

    /app/news/[slug] newsDetails page created

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -16,25 +16,50 @@ export default function NewsDetailsPage() {
     );
     setNewsData(mainNewsDataWithSlug!);
   }, []);
-  return newsData !== undefined ? <div className="w-[90%] mx-auto flex flex-col gap-y-5 mt-5">
-    <div className="w-full flex items-center gap-x-5">
-      <div className="flex flex-col items-start gap-y-3 w-[40%]">
-        <div className="text-blue-600 font-thin text-xl rounded-xl px-4 py-2 border border-blue-600">{newsData.cat}</div>
-        <h1 className="font-bold text-5xl leading-[65px]">{newsData.title}</h1>
-        <h2 className="font-thin text-neutral-800 text-xl leading-[35px]">{newsData.description}</h2>
-        <div className="flex items-center gap-x-2">
-          <div style={{
-            backgroundImage: `url(${newsData.picture})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }} className="rounded-full w-12 h-12 border border-neutral-300 mt-2"></div>
-          <section className="flex flex-col gap-y-1">
-            <h1 className="font-thin text-neutral-800 text-sm">نوشته شده توسط حمیدرضا قناعتیان</h1>
-            <h1 className="font-thin text-neutral-800 text-sm">تاریخ انتشار: {convertToFarsiNumbers(newsData.date.toString())}</h1>
-          </section>
+  return newsData !== undefined ? (
+    <div className="w-[90%] mx-auto flex flex-col gap-y-5 mt-5">
+      <div className="w-full flex items-center gap-x-5">
+        <div className="flex flex-col items-start gap-y-3 w-[40%]">
+          <div className="text-blue-600 font-thin text-xl rounded-xl px-4 py-2 border border-blue-600">
+            {newsData.cat}
+          </div>
+          <h1 className="font-bold text-5xl leading-[65px]">
+            {newsData.title}
+          </h1>
+          <h2 className="font-thin text-neutral-800 text-xl leading-[35px]">
+            {newsData.description}
+          </h2>
+          <div className="flex items-center gap-x-2">
+            <div
+              style={{
+                backgroundImage: `url(${newsData.picture})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+              className="rounded-full w-12 h-12 border border-neutral-300 mt-2"
+            ></div>
+            <section className="flex flex-col gap-y-1">
+              <h1 className="font-thin text-neutral-800 text-sm">
+                نوشته شده توسط حمیدرضا قناعتیان
+              </h1>
+              <h1 className="font-thin text-neutral-800 text-sm">
+                تاریخ انتشار: {convertToFarsiNumbers(newsData.date.toString())}
+              </h1>
+            </section>
+          </div>
         </div>
+        <Image
+          unoptimized
+          width={800}
+          height={800}
+          className="rounded-xl w-[60%] h-auto"
+          src={newsData.picture}
+          alt={newsData.title}
+        />
       </div>
-      <Image unoptimized width={800} height={800} className="rounded-xl w-[60%] h-auto" src={newsData.picture} alt={newsData.title} />
+      <p className="mt-3 text-lg font-thin leading-[35px]">{newsData.details}</p>
     </div>
-  </div>: <h1 className="text-center font-bold">در حال دریافت اطلاعات مقاله</h1>;
+  ) : (
+    <h1 className="text-center font-bold">در حال دریافت اطلاعات مقاله</h1>
+  );
 }
